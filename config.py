b91c56b35bac540d9291a5ca744718d5df823d37
Eric Duong <eduong@email.arizona.edu>
2021-11-15 19:18:06 -0700

    setting up

---
+++ config.py
@@ -0,0 +1,13 @@
+# 0 for self tapping, 1 for collaborative tapping
+SESSION = [0, 1, 0, 1]
+
+SECONDS_PER_SESSION = [15.0, 15.0, 15.0, 15.0]
+
+# count down before start
+SECONDS_COUNT_DOWN = 5.0
+
+# network configuration
+SERVER_ADDR = "192.168.0.245"
+SERVER_RECV_PORT = "6060"
+SERVER_SEND_PORT = "6061"
+HEADER = 128
